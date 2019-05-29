ff858198b755bb3424099933ada755ae7a30abd2
xueyelongyin <33425773+xueyelongyin@users.noreply.github.com>
2019-05-29 11:16:13 +0800

    Update main.js

--- main/main.js
+++ main/main.js
@@ -1,20 +1,19 @@
 module.exports = function main(inputs) {
     // write your code here...
-	disp=distance(inputs['distance']);
-	timp=inputs['parktime']*0.25;
-	price=Math.round(disp+timp);
+	disp=distance(inputs['distance']);//distance price
+	timp=inputs['parkTime']*0.25;//wait price
+	price=Math.round(disp+timp//sum price
     return price;
 };
 
 function distance(dis){
-	var price=0;
-	if(0<dis<=2){
-		price=6;
-	}else if(2<dis<8){
-		price=6+(dis-2)*0.8;
-	}else if(2<dis<8){
-		price=8.8+(dis-8)*1;
+	console.log(0<dis<=2);
+	if(0<dis&&dis<=2){
+		pri=6;
+	}else if(2<dis&&dis<8){
+		pri=6+(dis-2)*0.8;
+	}else if(dis>=8){
+		pri=10.8+(dis-8)*1.2;
 	}
-	
-	return price;
+	return pri;
 }
